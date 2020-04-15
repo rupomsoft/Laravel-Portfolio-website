7d53bcaaec224fdcf0dd0d46a9ee9b59aa74ccd0
rupomsoft <33941621+rupomsoft@users.noreply.github.com>
2020-04-15 11:59:40 +0600

    Admin panel JS Code Beautify...
    Admin panel JS Code Beautify...

--- admin/public/js/custom.js
+++ admin/public/js/custom.js
@@ -1,92 +1,90 @@
-$(document).ready(function () {
-$('#VisitorDt').DataTable();
-$('.dataTables_length').addClass('bs-select');
+
+// Visitor Page Table
+$(document).ready(function() {
+    $('#VisitorDt').DataTable();
+    $('.dataTables_length').addClass('bs-select');
 });
 
 
 
-function getServicesData(){
-axios.get('/getServicesData')
 
-.then(function(response){
+//For Services Table 
+function getServicesData() {
+    axios.get('/getServicesData')
 
-if(response.status==200){
+        .then(function(response) {
 
-$('#mainDiv').removeClass('d-none');
-$('#loaderDiv').addClass('d-none');
+            if (response.status == 200) {
 
-$('#service_table').empty();
+                $('#mainDiv').removeClass('d-none');
+                $('#loaderDiv').addClass('d-none');
 
-var jsonData=response.data;
+                $('#service_table').empty();
 
-	$.each(jsonData,function(i,item){
-			$('<tr>').html(
-				"<td><img class='table-img' src="+jsonData[i].service_img +"></td>"	+		
-				"<td>"+ jsonData[i].service_name +"</td>" +
-				"<td>"+ jsonData[i].service_des +"</td>" +
-				"<td><a href=''><i class='fas fa-edit'></i></a></td>" +
-				"<td> <a  class='serviceDeleteBtn'  data-id="+jsonData[i].id+" ><i class='fas fa-trash-alt'></i></a></td>"
-				).appendTo('#service_table');
-	});
+                var jsonData = response.data;
 
-
-
-  $('.serviceDeleteBtn').click(function(){
-  		var id=$(this).data('id');
-  		$('#serviceDeleteId').html(id);
-        $('#deleteModal').modal('show');
-
-  })
-
-  $('#serviceDeleteConfirmBtn').click(function(){
-     var id= $('#serviceDeleteId').html();
-     getServiceDelete(id);
-  })
+                $.each(jsonData, function(i, item) {
+                    $('<tr>').html(
+                        "<td><img class='table-img' src=" + jsonData[i].service_img + "></td>" +
+                        "<td>" + jsonData[i].service_name + "</td>" +
+                        "<td>" + jsonData[i].service_des + "</td>" +
+                        "<td><a href=''><i class='fas fa-edit'></i></a></td>" +
+                        "<td> <a  class='serviceDeleteBtn'  data-id=" + jsonData[i].id + " ><i class='fas fa-trash-alt'></i></a></td>"
+                    ).appendTo('#service_table');
+                });
 
 
-   	
-}
+                 // Services Table Delete Icon Click
+                $('.serviceDeleteBtn').click(function() {
+                    var id = $(this).data('id');
+                    $('#serviceDeleteId').html(id);
+                    $('#deleteModal').modal('show');
 
-else{
+                })
 
-$('#loaderDiv').addClass('d-none');
-$('#WrongDiv').removeClass('d-none');
-
-}
-
-})
-.catch(function (error) {
-$('#loaderDiv').addClass('d-none');
-$('#WrongDiv').removeClass('d-none');
-});
-
-}
+                 // Services Delete Modal Yes Btn
+                $('#serviceDeleteConfirmBtn').click(function() {
+                    var id = $('#serviceDeleteId').html();
+                    ServiceDelete(id);
+                })
 
 
+            } else {
 
+                $('#loaderDiv').addClass('d-none');
+                $('#WrongDiv').removeClass('d-none');
 
+            }
 
+        })
+        .catch(function(error) {
+            $('#loaderDiv').addClass('d-none');
+            $('#WrongDiv').removeClass('d-none');
+        });
 
-function getServiceDelete(deleteID){
-axios.post('/ServiceDelete',{id:deleteID})
-.then(function(response){
-
-     if(response.data==1){
-      $('#deleteModal').modal('hide');
-      toastr.success('Delete Success');
-      getServicesData();
-     }
-     else{
-     	$('#deleteModal').modal('hide');
-     	 toastr.error('Delete Fail');
-         getServicesData();
-     }
-
-})
-.catch(function (error) {
-
-});
 }
 
 
 
+ // Services Delete
+function ServiceDelete(deleteID) {
+    axios.post('/ServiceDelete', {
+            id: deleteID
+        })
+        .then(function(response) {
+
+            if (response.data == 1) {
+                $('#deleteModal').modal('hide');
+                toastr.success('Delete Success');
+                getServicesData();
+            } else {
+                $('#deleteModal').modal('hide');
+                toastr.error('Delete Fail');
+                getServicesData();
+            }
+
+        })
+        .catch(function(error) {
+
+        });
+}
