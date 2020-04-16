f5c0db07a108c2bc18c8dbce69e22864929cd0eb
rupomsoft <33941621+rupomsoft@users.noreply.github.com>
2020-04-16 11:20:29 +0600

    Manages Courses Data Admin Panel
    Manages Courses Data Admin Panel

--- admin/public/js/custom.js
+++ admin/public/js/custom.js
@@ -1,3 +1,45 @@
+//For Services Table 
+function getCoursesData() {
+    axios.get('/getCoursesData')
+        .then(function(response) {
+            if (response.status == 200) {
+                $('#mainDivCourse').removeClass('d-none');
+                $('#loaderDivCourse').addClass('d-none');
+                $('#course_table').empty();
+                var jsonData = response.data;
+                $.each(jsonData, function(i, item) {
+                    $('<tr>').html(
+                        "<td>"+jsonData[i].course_name+"</td>" +
+                        "<td>"+jsonData[i].course_fee+"</td>" +
+                        "<td>"+jsonData[i].course_totalclass+"</td>" +
+                        "<td>"+jsonData[i].course_totalenroll+"</td>" +
+                  
+
+                        "<td><a  class='courseViewDetailsBtn' data-id=" + jsonData[i].id + "><i class='fas fa-eye'></i></a></td>" +
+
+                        "<td><a  class='courseEditBtn' data-id=" + jsonData[i].id + "><i class='fas fa-edit'></i></a></td>" +
+
+                        "<td><a  class='courseDeleteBtn'  data-id=" + jsonData[i].id +" ><i class='fas fa-trash-alt'></i></a></td>"
+
+                    ).appendTo('#course_table');
+                });
+
+
+
+            } else {
+
+                $('#loaderDivCourse').addClass('d-none');
+                $('#WrongDivCourse').removeClass('d-none');
+
+            }
+
+        })
+        .catch(function(error) {
+                $('#loaderDivCourse').addClass('d-none');
+                $('#WrongDivCourse').removeClass('d-none');
+        });
+
+}
 
 
 
--- admin/resources/views/Courses.blade.php
+++ admin/resources/views/Courses.blade.php
@@ -3,13 +3,12 @@
 
 
 
-<div class="container">
+<div id="mainDivCourse"  class="container  d-none">
 <div class="row">
 <div class="col-md-12 p-5">
 <table id="" class="table table-striped table-bordered" cellspacing="0" width="100%">
   <thead>
     <tr>
-
 	  <th class="th-sm">Name</th>
 	  <th class="th-sm">Fee</th>
 	  <th class="th-sm">Class</th>
@@ -19,30 +18,31 @@
 	  <th class="th-sm">Delete</th>
     </tr>
   </thead>
-  <tbody>
-  
-	<tr>
-	  <th class="th-sm">Name</th>
-	  <th class="th-sm">2000</th>
-	  <th class="th-sm">200</th>
-      <th class="th-sm">200</th>
-	  <th class="th-sm"><a href="" ><i class="fas fa-eye"></i></a></th>
-	  <th class="th-sm"><a href="" ><i class="fas fa-edit"></i></a></th>
-	  <th class="th-sm"><a href="" ><i class="fas fa-trash-alt"></i></a></th>
-    </tr>	
+  <tbody id="course_table">
 
-	
-	
-	
   </tbody>
 </table>
-
 </div>
 </div>
 </div>
 
 
+<div id="loaderDivCourse" class="container">
+<div class="row">
+<div class="col-md-12 text-center p-5">
+		<img class="loading-icon m-5" src="{{asset('images/loader.svg')}}">
+</div>
+</div>
+</div>
 
+
+<div id="WrongDivCourse" class="container d-none">
+<div class="row">
+<div class="col-md-12 text-center p-5">
+		<h3>Something Went Wrong !</h3>
+</div>
+</div>
+</div>
 
 
 
@@ -54,7 +54,7 @@
 <script type="text/javascript">
 
 
-
+getCoursesData();
 
 </script>
 @endsection
