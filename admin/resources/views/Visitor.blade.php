4da35b2dc21fce0156a22eeddcfd88b2ffa8b6d7
rupomsoft <33941621+rupomsoft@users.noreply.github.com>
2020-04-16 07:14:40 +0600

    Admin Panel JS Code Re-Decorated
    Admin Panel JS Code Re-Decorated

--- admin/resources/views/Visitor.blade.php
+++ admin/resources/views/Visitor.blade.php
@@ -32,3 +32,20 @@
 
 
 @endsection
+
+
+
+@section('script')
+<script type="text/javascript">
+
+
+// Visitor Page Table
+$(document).ready(function() {
+    $('#VisitorDt').DataTable();
+    $('.dataTables_length').addClass('bs-select');
+});
+
+
+
+</script>
+@endsection
